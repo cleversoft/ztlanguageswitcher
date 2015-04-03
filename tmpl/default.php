2afdefc95a4a624830478013e6644f65da0cc36c
chinhbeo <chinhlv91@gmail.com>
2015-04-03 11:29:30 +0700

    ^ Update style and icon awesome

--- tmpl/default.php
+++ tmpl/default.php
@@ -10,6 +10,7 @@ defined('_JEXEC') or die;
 
 ?>
 <link href="<?php echo JURI::base(); ?>/modules/mod_zt_languageswitcher/assets/css/selectbox.css" type="text/css" rel="stylesheet" />
+<link href="<?php echo JURI::base(); ?>/modules/mod_zt_languageswitcher/assets/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
 
 <script type="text/javascript" src="<?php echo JURI::base(); ?>/modules/mod_zt_languageswitcher/assets/js/jquery.selectbox.js"></script>
 <div class="mod-languages<?php echo $moduleclass_sfx ?>">
